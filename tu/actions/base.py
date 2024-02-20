bb3b006c7143e2862ae79284c25b5413a1ac1f3d
Xitong Gao <gxtfmx@gmail.com>
2024-02-21 02:33:08 +0800

    Refactor common args

--- tu/actions/base.py
+++ tu/actions/base.py
@@ -1,40 +1,12 @@
-import os
-import copy
 from abc import abstractmethod
 
-import tomlkit
-
 from ..backends import BACKENDS
-from ..common import dict_merge
 
 
 class ActionBase:
-    base_options = {
-        ('-rc', '--rc-file'): {
-            'type': str,
-            'default': None,
-            'help':
-                'The configuration file to use. '
-                'If not provided, it reads from "~/.config/tu.toml" '
-                'and "./.tu.toml".'
-        },
-        ('-b', '--backend'): {
-            'type': str,
-            'default': None,
-            'choices': list(BACKENDS.keys()),
-            'help': 'The backend to use.',
-        },
-        ('-g', '--group'): {
-            'type': str,
-            'default': None,
-            'help': 'The group to use.',
-        },
-    }
-    rc_path = '.tu.toml'
-
     def __init__(self, name, parser_kwargs):
         super().__init__()
-        self.options = copy.deepcopy(self.base_options)
+        self.options = {}
         self.name = name
         self.parser_kwargs = parser_kwargs
 
@@ -45,56 +17,13 @@ class ActionBase:
     def main(self, args):
         raise NotImplementedError
 
-    def _load_config(self, args):
-        if args.rc_file:
-            with open(args.rc_file, 'r', encoding='utf-8') as f:
-                try:
-                    return tomlkit.load(f)
-                except tomlkit.exceptions.ParseError as e:
-                    print(f'Error parsing {args.rc_file}: {e}')
-                    return {}
-        else:
-            args.rc_file = self.rc_path
-        rc_files = [
-            os.path.join(os.path.dirname(__file__), '..', 'default.toml'),
-            '~/.config/tu.toml',
-            self.rc_path,
-        ]
-        config = {}
-        for path in rc_files:
-            try:
-                with open(path, 'r', encoding='utf-8') as f:
-                    try:
-                        rc = tomlkit.load(f)
-                    except tomlkit.exceptions.ParseError as e:
-                        print(f'Error parsing {path}: {e}')
-                        continue
-                config = dict_merge(config, rc)
-            except FileNotFoundError:
-                pass
-        return config
-
-    def _update_config(self, config, backend, group):
-        backend_config = config.get('backends', {}).get(backend, {})
-        group_config = config.get('groups', {}).get(group, {})
-        dict_merge(config, backend_config)
-        dict_merge(config, group_config)
-        config.update({
-            'backend': backend,
-            'group': group,
-        })
-        return config
-
-    def __call__(self, args):
+    def __call__(self, args, config):
         args = self.transform_args(args)
-        config = self._load_config(args)
-        backend_name = args.backend or config.get('backend', 'ts')
-        group_name = args.group or config.get('group', 'default')
-        config = self._update_config(config, backend_name, group_name)
+        backend = config['backend']
         try:
-            backend_cls = BACKENDS[backend_name]
+            backend_cls = BACKENDS[backend]
         except KeyError:
-            print(f'Invalid backend: {backend_name}')
+            print(f'Invalid backend: {backend}')
             backend_cls = BACKENDS['dummy']
         self.backend = backend_cls(config) if backend_cls else None
         return self.main(args)
--- tu/cli.py
+++ tu/cli.py
@@ -1,21 +1,91 @@
+import os
 import sys
 import argparse
 
+import tomlkit
+
+from .common import dict_merge
 from .actions import INFO
+from .backends import BACKENDS
 
 
 class CLI:
-    def __init__(self):
-        self.parser = argparse.ArgumentParser()
-        self.action_parsers = self.parser.add_subparsers(dest='action')
-        self.init_parser()
+    base_options = {
+        ('-rc', '--rc-file'): {
+            'type': str,
+            'default': None,
+            'help':
+                'The configuration file to use. '
+                'If not provided, it reads from "~/.config/tu.toml" '
+                'and "./.tu.toml".'
+        },
+        ('-b', '--backend'): {
+            'type': str,
+            'default': None,
+            'choices': list(BACKENDS.keys()),
+            'help': 'The backend to use.',
+        },
+        ('-g', '--group'): {
+            'type': str,
+            'default': None,
+            'help': 'The group to use.',
+        },
+    }
+    rc_path = '.tu.toml'
 
-    def init_parser(self):
+    def __init__(self):
+        super().__init__()
+        self.parser = argparse.ArgumentParser()
+        for option, kwargs in self.base_options.items():
+            self.parser.add_argument(*option, **kwargs)
+        action_parsers = self.parser.add_subparsers(dest='action')
         for action in INFO['actions'].values():
             kwargs = action.parser_kwargs
-            action_parser = self.action_parsers.add_parser(**kwargs)
+            action_parser = action_parsers.add_parser(**kwargs)
             for option, kwargs in action.options.items():
                 action_parser.add_argument(*option, **kwargs)
+
+    def _load_config(self, args):
+        if args.rc_file:
+            with open(args.rc_file, 'r', encoding='utf-8') as f:
+                try:
+                    return tomlkit.load(f)
+                except tomlkit.exceptions.ParseError as e:
+                    print(f'Error parsing {args.rc_file}: {e}')
+                    return {}
+        else:
+            args.rc_file = self.rc_path
+        rc_files = [
+            os.path.join(os.path.dirname(__file__), '..', 'default.toml'),
+            '~/.config/tu.toml',
+            self.rc_path,
+        ]
+        config = {}
+        for path in rc_files:
+            try:
+                with open(path, 'r', encoding='utf-8') as f:
+                    try:
+                        rc = tomlkit.load(f)
+                    except tomlkit.exceptions.ParseError as e:
+                        print(f'Error parsing {path}: {e}')
+                        continue
+                config = dict_merge(config, rc)
+            except FileNotFoundError:
+                pass
+        return config
+
+    def _update_config(self, args, config):
+        backend = args.backend or config.get('backend', 'ts')
+        group = args.group or config.get('group', 'default')
+        backend_config = config.get('backends', {}).get(backend, {})
+        group_config = config.get('groups', {}).get(group, {})
+        dict_merge(config, backend_config)
+        dict_merge(config, group_config)
+        config.update({
+            'backend': backend,
+            'group': group,
+        })
+        return config
 
     def main(self, args=None):
         args = args or sys.argv[1:]
@@ -23,15 +93,17 @@ class CLI:
             if '-h' in args or '--help' in args:
                 self.parser.print_help()
                 sys.exit(0)
-            args = [INFO['default']] + args
+            args += [INFO['default']]
         args = self.parser.parse_args(args)
+        config = self._load_config(args)
+        config = self._update_config(args, config)
         try:
             action = INFO['aliases'][args.action]
             action_func = INFO['actions'][action]
         except KeyError:
             print(f'Invalid action: {args.action}')
             sys.exit(1)
-        return action_func(args)
+        return action_func(args, config)
 
 
 def main():
