c140fd02035040463c473cd4b41f847c49cb4036
Xitong Gao <238821+admk@users.noreply.github.com>
2024-04-08 21:57:49 +0800

    Dummy backend

--- taskq/backends/dummy.py
+++ taskq/backends/dummy.py
@@ -3,35 +3,42 @@ from .base import register_backend, BackendBase
 
 @register_backend('dummy')
 class DummyBackend(BackendBase):
+    def __init__(self, name, config):
+        config['command'] = None
+        super().__init__(name, config)
+
     def backend_info(self):
         return {
             'name': 'dummy',
-            'command': None,
+            'command': '',
             'version': '0.0.0',
         }
 
     def backend_kill(self, args):
-        pass
+        print(args)
 
     def backend_command(self, command, commit=True):
-        return command
+        return command, commit
 
     def job_info(self, ids=None, filters=None):
+        print(ids, filters)
         return []
 
     def full_info(
         self, ids=None, filters=None, extra_func=None, tqdm_disable=False
     ):
+        print(ids, filters)
         return []
 
     def output(self, info, tail, shell=False):
-        pass
+        print(info, tail, shell)
 
     def add(self, command, gpus, slots, commit=True):
+        print(repr(command), gpus, slots, commit)
         return 0
 
     def kill(self, info, commit=True):
-        pass
+        print(info, commit)
 
     def remove(self, info, commit=True):
-        pass
+        print(info, commit)
